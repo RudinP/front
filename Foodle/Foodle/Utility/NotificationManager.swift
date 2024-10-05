70b16d10c793da53ae8e77ef2427e351b86f175a
RudinP <song_e@sookmyung.ac.kr>
2024-10-05 12:27:16 +0900

    매일 0시마다 약속을 fetch해서 갯수가 증가했다면 새로운 약속이 있음을 알리는 기능 추가

---
+++ Foodle/Foodle/Utility/NotificationManager.swift
@@ -0,0 +1,16 @@
+import UserNotifications
+
+class NotificationManager {
+    static let shared = NotificationManager()
+
+    func requestAuthorization() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if granted {
+                print("Notification permission granted.")
+            } else {
+                print("Notification permission denied.")
+            }
+        }
+    }
+}
